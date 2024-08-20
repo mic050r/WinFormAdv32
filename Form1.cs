79657bca4f1a131877261c7763ab945103625da5
mic050r <s2216@e-mirim.hs.kr>
2024-08-20 10:03:56 +0900

    Feat : 도움말 > 도움말 보기 기능 구현

--- Form1.cs
+++ Form1.cs
@@ -53,5 +53,11 @@ namespace WinFormAdv32
             textBox1.Text = "새로 만들기 메뉴를 선택하였습니다.";
         }
 
+        private void 도움말보기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://support.microsoft.com/ko-kr/windows/%EB%A9%94%EB%AA%A8%EC%9E%A5%EC%9D%98-%EB%8F%84%EC%9B%80%EB%A7%90-4d68c388-2ff2-0e7f-b706-35fb2ab88a8c");
+
+        }
+
     }
 }
